7ecf75b640eee0fcbffb76c3b4c9cae8ead5bc76
armchaircaver <arthur.vause@gmail.com>
2022-02-09 16:36:24 +0000

    Add cases to make up to 17723

--- nerdle_solution_generator.py
+++ nerdle_solution_generator.py
@@ -16,7 +16,7 @@ assumtions:
 
 - division by 1 is not allowed
 
-- d is positive (i.e. d>0)
+- d is >= 0)
 
 """
 from itertools import product
@@ -29,10 +29,10 @@ def generate_solutions():
       continue
 
     for x,y in product('+-*', repeat=2):
-      if not( x=='*' and (a==1 or b==1)) and not(y=='*' and (b==1 or c==1)):
+        #if not( x=='*' and (a==1 or b==1)) and not(y=='*' and (b==1 or c==1)):
         expression = str(a)+x+str(b)+y+str(c)
         d = eval(expression)
-        if d > 0:
+        if d >= 0:
           equation = expression+'='+str(d)
           if len(equation)==8:
             yield equation
@@ -42,7 +42,7 @@ def generate_solutions():
     # handle division with several separate cases
     x= '/'
     y= '*'
-    if b!=0 and b!=1 and (a*c)%b == 0 and not(y=='*' and c==1 ) :
+    if b!=0   and (a*c)%b == 0  :
 
       d = (a*c)//b
       expression = str(a)+x+str(b)+y+str(c)
@@ -51,12 +51,23 @@ def generate_solutions():
         yield equation
         #print(equation)
 
+    x= '*'
+    y= '/'
+    if c!=0   and (a*b)%c == 0  :
+
+      d = (a*b)//c
+      expression = str(a)+x+str(b)+y+str(c)
+      equation = expression+'='+str(d)
+      if len(equation)==8:
+        yield equation
+        #print(equation)
+
     x='/'
-    if b!=0 and b!=1 and (a)%b == 0  :
+    if b!=0 and (a)%b == 0  :
 
       for y in '+-':
         d= a//b + c if y=='+' else a//b - c
-        if d > 0:
+        if d >= 0:
           expression = str(a)+x+str(b)+y+str(c)
           equation = expression+'='+str(d)
           if len(equation)==8:
@@ -65,7 +76,7 @@ def generate_solutions():
           
     x='/'
     y='/'
-    if b*c != 0 and b!=1 and c!=1 and a%(b*c)==0:
+    if b*c != 0 and a%(b*c)==0:
       d = a//(b*c)
       expression = str(a)+x+str(b)+y+str(c)
       equation = expression+'='+str(d)
@@ -74,11 +85,11 @@ def generate_solutions():
         #print(equation)
     
     y='/'
-    if c!=0 and c!=1 and b%c == 0  :
+    if c!=0  and b%c == 0  :
 
       for x in '+-':
         d= a+b//c if x=='+' else a-b//c
-        if d > 0:
+        if d >= 0:
           expression = str(a)+x+str(b)+y+str(c)
           equation = expression+'='+str(d)
           if len(equation)==8:
@@ -91,17 +102,16 @@ def generate_solutions():
       continue
 
     for x in '+-*':
-      if not( x=='*' and (a==1 or b==1) ):
         expression = str(a)+x+str(b)
         d = eval( expression )
-        if d > 0 :
+        if d >= 0 :
           equation = expression+'='+str(d)
           if len(equation)==8:
             yield equation
             #print(equation)
       
     x='/'
-    if b>0 and b!=1 and a%b==0:
+    if b>0 and a%b==0:
       d= a//b
       equation = str(a)+x+str(b)+'='+str(d)
       if len(equation)==8:
